e30b30ef8c15b079b3aa5a96b988694251beb6c9
Tavis Weeden <tavisweeden@gmail.com>
2019-10-01 23:30:29 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,8 +24,7 @@ def bonus
       
 
 #code your solution here:
-
-  
+epic_tragedy[:montague][:hero][:Romeo] = 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
